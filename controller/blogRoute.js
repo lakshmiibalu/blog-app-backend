01ff1ca4046e1952415fcd1025b0d2a2cd6ebb00
lakshmiibalu <lakshmibalu74@gmail.com>
2024-02-09 12:23:27 +0530

    signin created

--- controller/blogRoute.js
+++ controller/blogRoute.js
@@ -13,21 +13,54 @@ router.post("/signup",async(req,res)=>{
     
     let {data} = {"data":req.body}
     let password = data.password
-    hashPasswordgenerator(password).then(
-        (hashedPassword)=>{
-            console.log(hashedPassword)
-            data.password=hashedPassword
-            console.log(data)
-            let user = new users(data)
-        let result =  user.save()
-        res.json(
-            {
-                status:"success"
-            }
+    const hashedPassword = await hashPasswordgenerator(password)
+    data.password=hashedPassword
+    let user = new users(data)
+            let result = await user.save()
+             res.json(
+                {
+                    status:"success"
+                }
+                )
+            })
+
+        
+
+     router.post("/signin",async(req,res)=>{
+         let input = req.body
+         let email = req.body.email
+        let data = await users.findOne({"email":email})
+
+        if(!data)
+        {
+            return res.json(
+                {
+                    status:"invalid user"
+                }
             )
-        })
         }
-    )
+
+        console.log(data)
+        let dbPassword = data.password
+        let inputPassword = req.body.password
+        console.log(dbPassword)
+        console.log(inputPassword)
+
+        const match = await bcrypt.compare(inputPassword,dbPassword)
+
+        if (!match) {
+            return res.json({
+                status:"incorrect password"
+            })
+            
+        }
+
+        res.json({
+            status:"success"
+        })
+        
+
+     })
 
     
 
