d857456dafcd94435a5d5f419aa97f9f503a4634
lakshmiibalu <lakshmibalu74@gmail.com>
2024-02-09 11:27:09 +0530

    security created

--- controller/blogRoute.js
+++ controller/blogRoute.js
@@ -3,15 +3,32 @@ const users = require("../models/users")
 
 const router = express.Router()
 
+const bcrypt = require("bcryptjs")
+hashPasswordgenerator=async(pass)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
 router.post("/signup",async(req,res)=>{
-    let data = req.body
-    let user = new users(data)
-    let result = await user.save()
-    res.json(
-        {
-            status:"success"
+    
+    let {data} = {"data":req.body}
+    let password = data.password
+    hashPasswordgenerator(password).then(
+        (hashedPassword)=>{
+            console.log(hashedPassword)
+            data.password=hashedPassword
+            console.log(data)
+            let user = new users(data)
+        let result =  user.save()
+        res.json(
+            {
+                status:"success"
+            }
+            )
+        })
         }
     )
-})
+
+    
 
 module.exports = router
